dd2967900987658b0b4bb360fd07cccf05b97ace
NavodContrath <navod.contrath@gmail.com>
2025-03-20 23:25:04 +0100

    Add Footer component and integrate it into App; update CSS for footer styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
