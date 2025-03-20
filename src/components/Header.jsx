834f114f08607573912b448725a31366f21b65b2
NavodContrath <navod.contrath@gmail.com>
2025-03-20 22:33:21 +0100

    Add in Header component a navigation menu and responsive styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 export default function Header() {
     return (
         <header>
-            <img src="./public/img/dc-logo.png" alt="" />
-        </header>
+            <div className="container d-flex" id="navBar">
+                <a href=""><img src="./public/img/dc-logo.png" alt="" /></a>
+                <div id="nav-menu" className="d-flex">
+                    <a href="">CHARACTERS</a>
+                    <a href="" style={{ color: "#0282f9", borderBottom: "5px solid #0282f9" }}>COMICS</a>
+                    <a href="">MOVIES</a>
+                    <a href="">TV</a>
+                    <a href="">GAMES</a>
+                    <a href="">COLLECTIBLES</a>
+                    <a href="">VIDEOS</a>
+                    <a href="">FANS</a>
+                    <a href="">NEWS</a>
+                    <a href="">SHOP</a>
+                </div>
+            </div >
+        </header >
     )
 }
