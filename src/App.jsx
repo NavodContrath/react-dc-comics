6769dbc9403034ef0394b98ff371969ec5c8d52b
NavodContrath <navod.contrath@gmail.com>
2025-03-20 21:19:46 +0100

    Refactor App component to use Header component and add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/Header'
 
 
 function App() {
 
   return (
     <>
-      <header>ciaone</header>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./public/img/dc-logo.png" alt="" />
+        </header>
+    )
+}
