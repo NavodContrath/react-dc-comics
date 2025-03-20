32613d08b39183cf03273146bb0809ad8f9b6551
NavodContrath <navod.contrath@gmail.com>
2025-03-21 00:13:02 +0100

    Add Footer component with links and social sections; update CSS for layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,60 @@
 export default function Footer() {
     return (
         <footer>
+            <section id="our-links">
+                <div className="row container">
+                    <div className="col-5 d-flex">
+                        <div>
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                            </ul>
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>DC</h3>
+                            <ul>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>SITES</h3>
+                            <ul>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                                <li><a href="">Lorem</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-7" style={{ backgroundImage: "URL('/img/dc-logo-bg.png')", backgroundPosition: "center", backgroundRepeat: "no-repeat", minHeight: "100%" }}></div>
+                </div>
+            </section>
+            <section id="our-social">
+                <div className="container">
 
+                </div>
+            </section>
         </footer>
     )
 }
