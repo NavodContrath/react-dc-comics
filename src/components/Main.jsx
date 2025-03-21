4fc1078611135ab445c41667d8208c3595efb53d
NavodContrath <navod.contrath@gmail.com>
2025-03-21 17:14:26 +0100

    refactor Main component to simplify toolbar structure and improve accessibility

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,11 @@ export default function Main() {
             </div>
             <div id="tool-bar" >
                 <div className="container row">
-                    <div className="col-2 d-flex"><img src="img/buy-comics-digital-comics.png" alt="" /><a href="">DIGITAL COMICS</a></div>
-                    <div className="col-2 d-flex"><img src="img/buy-comics-merchandise.png" alt="" /><a href="">DC MERCHANDISE</a></div>
-                    <div className="col-2 d-flex"><img src="img/buy-comics-subscriptions.png" alt="" /><a href="">SUBSCRIPTIONS</a></div>
-                    <div className="col-2 d-flex"><img src="img/buy-comics-shop-locator.png" alt="" /><a href="">COMIC SHOP LOCATOR</a></div>
-                    <div className="col-2 d-flex"><img src="img/buy-dc-power-visa.svg" alt="" /><a href="">DC POWER VISA</a></div>
+                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-digital-comics.png" alt="" />DIGITAL COMICS</a>
+                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-merchandise.png" alt="" />DC MERCHANDISE</a>
+                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-subscriptions.png" alt="" />SUBSCRIPTIONS</a>
+                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-shop-locator.png" alt="" />COMIC SHOP LOCATOR</a>
+                    <a href="" className="col-2 d-flex"><img src="img/buy-dc-power-visa.svg" alt="" />DC POWER VISA</a>
                 </div>
             </div>
         </main>
