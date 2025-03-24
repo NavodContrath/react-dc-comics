16d77dfc7ed134ee2475b4f2354e9e4356573f24
NavodContrath <navod.contrath@gmail.com>
2025-03-24 11:46:25 +0100

    add menuLinks data structure for navigation

---
+++ src/components/data/links.js
@@ -0,0 +1,55 @@
+const menuLinks = [
+    {
+        id: 1,
+        name: "CHARACHTERS",
+        url: "#",
+        active: false
+    },
+    {
+        id: 2,
+        name: "CHARACHTERS",
+        url: "#",
+        active: true
+    }, {
+        id: 3,
+        name: "COMICS",
+        url: "#",
+        active: false
+    }, {
+        id: 4,
+        name: "TV",
+        url: "#",
+        active: false
+    }, {
+        id: 5,
+        name: "GAMES",
+        url: "#",
+        active: false
+    }, {
+        id: 6,
+        name: "COLLECTIBLES",
+        url: "#",
+        active: false
+    }, {
+        id: 7,
+        name: "VIDEOS",
+        url: "#",
+        active: false
+    }, {
+        id: 8,
+        name: "FANS",
+        url: "#",
+        active: false
+    }, {
+        id: 9,
+        name: "NEWS",
+        url: "#",
+        active: false
+    }, {
+        id: 10,
+        name: "SHOP",
+        url: "#",
+        active: false
+    },
+]
+export default menuLinks
