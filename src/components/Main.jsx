53596a4726bb92d04c87d6265dbc2bd52f2208ca
NavodContrath <navod.contrath@gmail.com>
2025-03-24 11:00:46 +0100

    refactor Main component layout and add box shadow for improved aesthetics

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,12 @@ export default function Main() {
             <div id="jumbotron" ></div>
             <ComicList />
             <div id="tool-bar" >
-                <div className="container row">
-                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-digital-comics.png" alt="" />DIGITAL COMICS</a>
-                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-merchandise.png" alt="" />DC MERCHANDISE</a>
-                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-subscriptions.png" alt="" />SUBSCRIPTIONS</a>
-                    <a href="" className="col-2 d-flex"><img src="img/buy-comics-shop-locator.png" alt="" />COMIC SHOP LOCATOR</a>
-                    <a href="" className="col-2 d-flex"><img src="img/buy-dc-power-visa.svg" alt="" />DC POWER VISA</a>
+                <div className="container d-flex">
+                    <a href="" className="d-flex"><img src="img/buy-comics-digital-comics.png" alt="" />DIGITAL COMICS</a>
+                    <a href="" className="d-flex"><img src="img/buy-comics-merchandise.png" alt="" />DC MERCHANDISE</a>
+                    <a href="" className="d-flex"><img src="img/buy-comics-subscriptions.png" alt="" />SUBSCRIPTIONS</a>
+                    <a href="" className="d-flex"><img src="img/buy-comics-shop-locator.png" alt="" />COMIC SHOP LOCATOR</a>
+                    <a href="" className="d-flex"><img src="img/buy-dc-power-visa.svg" alt="" />DC POWER VISA</a>
                 </div>
             </div>
         </main>
